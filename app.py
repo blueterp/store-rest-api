290b650ccde04f7ae09b2d9269b2d670a7ef4696
Jonathan Howarth <blueterp@gmail.com>
2023-04-27 22:05:58 -0400

    create endpoint for creating store and item

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
 
-stores = [{"name": "Walmart", "items": [{"name": "chair", "price": 14.99}]}]
+stores = [{"name": "walmart", "items": [{"name": "chair", "price": 14.99}]}]
 
 
-@app.route("/stores", methods=["GET"])
+@app.route("/store", methods=["GET", "POST"])
 def list_stores():
+    if request.method == "POST":
+        data = request.get_json()
+        name = data["name"]
+        items = []
+        new_store = {"name": name, "items": items}
+        stores.append(new_store)
+        return new_store, 201
     return {"stores": stores}
+
+
+@app.route("/store/<string:store_name>/item", methods=["POST"])
+def create_item(store_name):
+    data = request.get_json()
+    for store in stores:
+        if store["name"] == store_name:
+            new_item = {"name": data["name"], "price": data["price"]}
+            store["items"].append(new_item)
+        return new_item, 201
+
+    return {"message": "store not found"}, 404
