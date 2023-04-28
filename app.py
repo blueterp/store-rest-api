eb89c0cf4c60726a82c9d474ac9dd6abcebd858f
Jonathan Howarth <blueterp@gmail.com>
2023-04-27 21:44:38 -0400

    create simple store dataset and route for listing stores

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [{"name": "Walmart", "items": [{"name": "chair", "price": 14.99}]}]
 
-@app.route('/')
-def hello():
-    return "hello world from new app."
+
+@app.route("/stores", methods=["GET"])
+def list_stores():
+    return {"stores": stores}
